10c7883f321b9d2eba937b3df12efa30bf8f513e
Novaz <hello@benline.co.uk>
2017-09-02 00:12:46 +0100

    PHP - NOVAZ - HEADER.PHP - INDEX.PHP
    #HOLDMYBEER

---
+++ php/head.php
@@ -0,0 +1,49 @@
+<!--
+
+       ;
+       ;
+
+        t
+        t                            .
+        t                          t
+      ,tt  ,                    tjt
+     tt ttt                 ttttt
+  ttttjttttjt      .ttttttttttt
+  ttt tttttttt    ttttttttttt
+    t ttt ttttt ttttttttttt
+    tt   itttttttttttttttj
+    t     jtttttttttttttt
+           ttttttttttttt
+            ttttttt ttt
+            tttttt;jtt
+         tttt ttj.:tt
+         tt       ttt
+          t       ttttttj
+           jt           t
+            ttt         t
+                      ttt
+                      ttt
+                     ;  tt
+                         tt
+#HOLDMYBEER
+#NOVAZ
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <title>Gameforest - Gaming Theme HTML</title>
+    <!-- stylesheet -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
+    <link href="plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="plugins/animate/animate.min.css" rel="stylesheet">
+    <!-- plugins -->
+    <link href="plugins/owl-carousel/css/owl.carousel.css" rel="stylesheet">
+    <!-- theme stylesheet -->
+    <link href="css/theme.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+  </head>
